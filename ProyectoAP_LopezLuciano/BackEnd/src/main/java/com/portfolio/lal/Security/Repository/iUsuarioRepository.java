13fb519a47bc28131291dc47586ea9abbb99c001
Selgoku <luciano_014@hotmail.com>
2022-09-27 00:34:28 -0300

    Se continua configurando Login

---
+++ ProyectoAP_LopezLuciano/BackEnd/src/main/java/com/portfolio/lal/Security/Repository/iUsuarioRepository.java
@@ -0,0 +1,18 @@
+package com.portfolio.lal.Security.Repository;
+
+import com.portfolio.lal.Security.Entity.Usuario;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iUsuarioRepository extends JpaRepository<Usuario, Integer>{
+    Optional<Usuario> findByNombreUsuario(String nombreUsuario);
+    
+    boolean existByNombreUsuario(String nombreUsuario);
+    boolean existByNombreEmail(String email);
+
+    public boolean existsByNombreUsuario(String nombreUsuario);
+
+    public boolean existsByEmail(String email);
+}
---
+++ ProyectoAP_LopezLuciano/BackEnd/src/main/java/com/portfolio/lal/Security/Service/UserDetailImpl.java
@@ -0,0 +1,21 @@
+package com.portfolio.lal.Security.Service;
+
+import com.portfolio.lal.Security.Entity.Usuario;
+import com.portfolio.lal.Security.Entity.UsuarioPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailImpl implements UserDetailsService {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.getByNombreUsuario(nombreUsuario).get();
+        return UsuarioPrincipal.build(usuario);
+    }
+}
