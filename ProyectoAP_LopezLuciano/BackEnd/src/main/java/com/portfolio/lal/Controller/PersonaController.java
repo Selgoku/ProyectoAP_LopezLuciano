b1bc542b68b12a7c8c05c93843ddd392157187b1
Selgoku <luciano_014@hotmail.com>
2022-10-10 15:39:26 -0300

    Fix Actualizar "Acerca De"+ "Cambio foto de perfil"
    Se arreglo el editor de la seccion "Acerca De"
    Funciona tanto en version "Local" como en version "Produccion"

--- ProyectoAP_LopezLuciano/BackEnd/src/main/java/com/portfolio/lal/Controller/PersonaController.java
+++ ProyectoAP_LopezLuciano/BackEnd/src/main/java/com/portfolio/lal/Controller/PersonaController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -63,5 +64,18 @@ public class PersonaController {
         
         personaService.save(persona);
         return new ResponseEntity(new Mensaje("Persona Actualizada"), HttpStatus.OK);
+    }
+    
+    //Borrar Personas:
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        //Se pregunta si el ID existe
+        if (!personaService.existsById(id)) {
+            return new ResponseEntity(new Mensaje("El ID no existe"), HttpStatus.BAD_REQUEST);
+        }
+        
+        personaService.delete(id);
+        
+        return new ResponseEntity(new Mensaje("Persona eliminada"), HttpStatus.OK);
     }
 }
